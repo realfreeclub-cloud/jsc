d81424ea6988760b82eb9f89fa7c21b459c1f69b
realfreeclub-cloud <realfreeclub@gmail.com>
2026-05-20 00:23:51 +0530

    g

---
+++ admin/src/types/xlsx.d.ts
@@ -0,0 +1,7 @@
+declare module 'xlsx' {
+  export const utils: unknown;
+  export const read: unknown;
+  export const writeFile: unknown;
+  export const book_new: unknown;
+  export const book_append_sheet: unknown;
+}
